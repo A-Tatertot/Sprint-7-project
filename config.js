8950a89ee8b98d8ab2a925c8d6d2277d27e8ca5b
A-Tatertot <cftater22@gmail.com>
2024-10-30 22:32:35 -0400

    transfering local to github

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-3403747e-bc8d-4c21-89b4-1d573e972018.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-52953f20-6d99-474c-992a-174fb6fe12eb.containerhub.tripleten-services.com"
 }
